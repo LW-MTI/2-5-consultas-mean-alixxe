6cc36d2455d5c2c65352c2a987f18cb7ed04d8f0
MarthaAlicia <maalestradaal@ittepic.edu.mx>
2017-12-12 11:25:09 -0700

    121217

--- models/user.model.js
+++ models/user.model.js
@@ -4,21 +4,55 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    name: {
+    nombre:{
         type: String,
         required: true
-    }, 
+    },
+    apellido:{
+     type: String,
+     required: true
+    },
+    calle:{
+     type: String,
+     required: true
+    },
+    numero:{
+     type: String,
+     required: true
+    },
+    colonia:{
+     type: String,
+     required: true
+    },
+    codpostal:{
+     type: String,
+     required: true
+    },
+    estado:{
+     type: String,
+     required: true
+    },
+    tel_cel:{
+     type: Number_serv,
+     required: true
+    },     
     email: {
         type: String,
         required: true,
-        match: /.+@.+\..+/,
+        match: /.+@.+\.+/,
         lowercase: true
     },
     password: {
-        type: String,
-        required: true
-    }
-});
+     type: String,
+    require: true 
+    }, 
+    prod_serv:{
+     type: String,
+     required: true
+    },
+  
+ });
+
 
 const userModel = mongoose.model('User', userSchema, 'users');
 
--- router/user.router.js
+++ router/user.router.js
@@ -3,14 +3,15 @@ const handler = require('../utils/handler');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/unidad02', {
+//conecta con la base de datos
+mongoose.connect('mongodb://localhost:27017/unidad2', {
   useMongoClient: true
 });
 
 const User = require('../models/user.model');
 
 module.exports = () => {
-
+//siempre es igual
     router.get('/', (req, res) => {
         User.find({})
         .sort()
@@ -22,6 +23,79 @@ module.exports = () => {
         User.find({_id:id})
         .sort()
         .exec(handler.handleMany.bind(null, 'users', res));
+    });
+
+    
+    router.get('/name/:name', (req, res) => {
+        const nombre = req.params.name;
+        User.find({name:nombre})
+        .sort()
+        .exec(handler.handleMany.bind(null, 'users', res));
+    });
+
+    router.get('/email/email:', (req, res) => {
+        const correo = req.params.email;
+        User.find({email:correo})
+        .sort()
+        .exec(handler.handleMany.bind(null, 'users', res));
+    });
+
+
+/*----Inserciones---*/
+
+router.post('/', (req, res) => {
+    //Recibir parametros
+
+    const usuario =  req.body;
+    
+    User.create(usuario)
+        .then(
+            function(data){
+                console.log(data);
+                res.json(data);
+
+            }
+        )
+        .catch(
+            function (err){
+                console.log(err);
+                res.status(400);
+                res.json({error:err});
+            }
+        )
+    });
+   
+    /* Eliminar */
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id;
+                
+        User.remove({_id:id}, function(err,data){
+            if(err){
+                console.log(err);
+                res.status(400);
+                res.json({error:err});
+            }else{
+                res.json(data);
+            }
+            
+        })
+        
+    });
+
+ //modificar
+ router.put('/:id', (req, res) => {
+    var id = args._id;
+    var updateObj = {updatedDate: Date.now()};
+    _.extend(updateObj, args);
+
+    Model.findByIdAndUpdate(id, updateObj, function(err, model) {
+        if (err) {
+            logger.error(modelString +':edit' + modelString +' - ' + err.message);
+            self.emit('item:failure', 'Failed to edit ' + modelString);
+            return;
+        }
+        self.emit('item:success', model);
+    });
     });
 
     return router;
